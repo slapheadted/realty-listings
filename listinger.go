0ac6cd2357f4a6e278e73bd222e738fdbf150e9f
Chris Saunders <chris.saunders@clio.com>
2017-10-01 20:22:53 -0700

    Converted tabs to spaces

--- listinger.go
+++ listinger.go
@@ -2,5 +2,5 @@ package main
 
 // Listinger is the common interface for a property listing
 type Listinger interface {
-	getName() string
+  getName() string
 }
--- point2homes.go
+++ point2homes.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"encoding/json"
-	"github.com/parnurzeal/gorequest"
-	"log"
+  "fmt"
+  "encoding/json"
+  "github.com/parnurzeal/gorequest"
+  "log"
 )
 
 type listingItem struct {
-	ListingID int    `json:"Listing_Id"`
-	Address   string `json:"Address"`
+  ListingID int    `json:"Listing_Id"`
+  Address   string `json:"Address"`
 }
 
 type listingsList struct {
-	ListingsSummaryVM []listingItem `json:"ListingsSummaryVM"`
+  ListingsSummaryVM []listingItem `json:"ListingsSummaryVM"`
 }
 
 type listingResponse struct {
-	ListingsList listingsList `json:"ListingsList"`
+  ListingsList listingsList `json:"ListingsList"`
 }
 
 func (l listingItem) getName() string {
-	return l.Address
+  return l.Address
 }
 
 func fetchListings() []listingItem {
-	request := gorequest.New()
+  request := gorequest.New()
   _, body, errs := request.Post("https://www.point2homes.com/CA/Real-Estate-Listings.html").
     Send("&location=Kootenay+Rockies,+BC&search_mode=location&PriceMin=150000&PriceMax=450000&LotSizeMin=5&page=1&SelectedView=listings&LocationGeoId=432040&ajax=1").
     End()
 
-	if len(errs) > 0 {
+  if len(errs) > 0 {
     fmt.Println(errs)
-	}
+  }
 
-	var cont listingResponse
+  var cont listingResponse
 
-	if err := json.Unmarshal([]byte(body), &cont); err != nil {
-		log.Fatal(err)
-	}
+  if err := json.Unmarshal([]byte(body), &cont); err != nil {
+    log.Fatal(err)
+  }
 
-	var singleListing = cont.ListingsList.ListingsSummaryVM[0]
-	fmt.Println("single", singleListing.getName())
+  var singleListing = cont.ListingsList.ListingsSummaryVM[0]
+  fmt.Println("single", singleListing.getName())
 
   return cont.ListingsList.ListingsSummaryVM
 }
